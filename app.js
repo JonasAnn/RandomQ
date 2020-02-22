b9875984efaf72f7420dd4536705a82b2d789e92
Jonah Oluwapelumi Ifeoma <ifyajoke@gmail.com>
2020-02-23 00:51:55 +0100

    html and css done and js added

---
+++ app.js
@@ -0,0 +1,55 @@
+wordArr = ['If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.', 
+'The person who says it cannot be done should not interrupt the person who is doing it.', 
+'Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.', 
+'The question isn’t who is going to let me; it’s who is going to stop me.', 
+'When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.', 
+'The two most important days in your life are the day you are born and the day you find out why.',
+'The most common way people give up their power is by thinking they don’t have any.',
+'Challenges are what make life interesting and overcoming them is what makes life meaningful.',
+'I have learned over the years that when one’s mind is made up, this diminishes fear.'];
+
+authorArr = ['King Luther', 'Marvellous Kelvin', 'Nelly J. Man', 'Goodman Love', 'Mary Gate', 'John Jazz', 'Mike Martin', 'Becky Ball', 'Arial Sing'];
+
+colorArr = ['#FF0000'	, '#800000', '#FFFF00', 	'#808000', 	'#00FF00', '#008000', '#00FFFF', 	'#008080', '#0000FF'];
+
+// gove.forEach(function(gove, index) {
+//   gove.textContent = `${index} : ${"love"}`;
+// });
+
+const buttonClick = document.getElementById("new-quote");
+const authorText = document.getElementById("text");
+const authorName = document.getElementById("author");
+const colorChain = document.querySelectorAll(".color-change");
+
+buttonClick.addEventListener("click", newEvent);
+
+  function newEvent(e){
+    for (let t = 0; t < wordArr.length; t++) {
+    authorText.textContent = wordArr[t];
+  }
+  
+};
+
+
+// buttonClick.addEventListener('click', nameEvent);
+
+// function nameEvent(e) {
+//   colorChain.style.color = "red"
+//   e.preventDefault();
+// };
+
+// for ( let c = 0; c < colorArr.length; c++) {
+//   console.log(colorArr[c]);
+// }
+
+// function colorEvent(e) {
+
+//   colorChain.style.color = "red"
+//   e.preventDefault();
+// };
+
+// let val; 
+
+// val = colorArr.length;
+
+// console.log(val);
